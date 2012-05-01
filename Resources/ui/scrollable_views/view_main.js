97f0f060b4842289d242875f371d8a500b0173c5
Yoann GAUCHARD <yoanngauchard@new-host.home>
2012-05-01 16:25:07 +0200

    [REF] change position from location update

--- Resources/ui/scrollable_views/view_main.js
+++ Resources/ui/scrollable_views/view_main.js
@@ -25,28 +25,13 @@ var anno_current = Ti.Map.createAnnotation({
 	longitude : svc_geo.LONGITUDE_BASE,
 });
 
-
+svc_geo.monitorGPSPosition();
 
 Ti.App.addEventListener('evtLocationUpdate', function(obj_coords){
 	var tab_anno = view_main.getAnnotations();
-	tab_anno[0].latitude = 38.490000;
-	tab_anno[0].longitude = -121.050210;
+	tab_anno[0].latitude = obj_coords.latitude;
+	tab_anno[0].longitude = obj_coords.longitude;
 	view_main.setAnnotations(tab_anno);
-	view_main.setRegion({
-		latitude : "38.49",
-    	latitudeDelta : "0.1",
-    	longitude : "-121.050210",
-    	longitudeDelta : "0.1",
-	});
-	Ti.API.info(view_main.getRegion());
-});
-
-anno_current.addEventListener('click', function(e){
-	
-	setTimeout(function(){
-		svc_geo.monitorGPSPosition()
-	},2000);
-	
 });
 
 view_main.addAnnotation(anno_current);
